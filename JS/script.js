26fcf5a874769fab6ad3b13da1d10322a7457936
Akotilla <etienne.viala@gmail.com>
2025-07-29 13:07:34 +0200

    mise en page JS preferences et stockage en local

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,51 @@
-document.getElementById("enregistrer").addEventListener("click", store);
+var theme;
+var affichage;
 
-function store() {
-    storage.local.set();
+function init() {
+    check();
+    document
+        .getElementById("enregistrer")
+        .addEventListener("click", function (event) {
+            event.preventDefault();
+            get();
+        });
 }
+
+//vérifier les valeurs localStorage
+function check() {
+    if (localStorage.getItem("affichage")) {
+        affichage = localStorage.getItem("affichage");
+        if (affichage === "liste") {
+            document.getElementById("liste").checked = true;
+        } else if (affichage === "cartes") {
+            document.getElementById("cartes").checked = true;
+        }
+    }
+    if (localStorage.getItem("theme")) {
+        theme = localStorage.getItem("theme");
+        document.getElementById("theme").value = theme;
+    }
+}
+
+// récupérer les valeurs au clic
+function get() {
+    let liste = document.getElementById("liste").checked;
+    let cartes = document.getElementById("cartes").checked;
+    theme = document.getElementById("theme").value;
+    console.log(liste);
+    if (liste) {
+        affichage = "liste";
+    }
+    if (cartes) {
+        affichage = "cartes";
+    }
+    store();
+}
+
+// stocker les valeurs localStorage
+function store() {
+    localStorage.setItem("affichage", affichage);
+    localStorage.setItem("theme", theme);
+}
+
+init();
