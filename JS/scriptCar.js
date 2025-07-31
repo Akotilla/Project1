15a04975bfc97eac3fb4e89fd8a855f29346300d
Akotilla <etienne.viala@gmail.com>
2025-07-31 12:35:14 +0200

    optimisation JS scriptCar

--- JS/scriptCar.js
+++ JS/scriptCar.js
@@ -1,5 +1,4 @@
-var map = L.map("map").setView([46.860191, 2.592773], 6);
-var promotion;
+const map = L.map("map").setView([46.860191, 2.592773], 6);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
@@ -13,19 +12,14 @@ function promo() {
 }
 
 function afficher(data) {
-    console.log(data);
-    promotion = data.apprenants;
-    console.log(promotion);
-    mapMarker();
+    const promotion = data.apprenants;
+    mapMarker(promotion);
 }
-function mapMarker() {
-    console.log(promotion[0].coordonnees);
-
+function mapMarker(promotion) {
     promotion.forEach((element) => {
         let lat = parseFloat(element.coordonnees.latitude);
         let lng = parseFloat(element.coordonnees.longitude);
-        console.log("Nom:", element.nom, "Lat:", lat, "Lng:", lng);
-        // Exemple pour ajouter un marker sur la carte :
+
         L.marker([lat, lng])
             .addTo(map)
             .bindPopup(
