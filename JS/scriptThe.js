1116f6e4e0a336a15dae7a3d0829fb70de4808be
Akotilla <etienne.viala@gmail.com>
2025-08-01 15:17:30 +0200

    maj JSON et affichage

--- JS/scriptThe.js
+++ JS/scriptThe.js
@@ -16,3 +16,28 @@ function myFunction() {
         x.style.visibility = "hidden";
     }
 }
+
+function lecture() {
+    fetch("JS/promo.json")
+        .then((response) => response.json())
+        .then((data) => afficher(data));
+}
+
+function afficher(data) {
+    document.querySelector("h1").textContent = "Promo: " + data.nomPromotion;
+    if (document.querySelector(".dateDebut")) {
+        document.querySelector(".dateDebut").textContent = data.dateDebut;
+    }
+    if (document.querySelector(".dateFin")) {
+        document.querySelector(".dateFin").textContent = data.dateFin;
+    }
+    if (document.querySelector(".nbrApprenant")) {
+        document.querySelector(".nbrApprenant").textContent =
+            data.nbrApprenants;
+    }
+    if (document.querySelector(".description")) {
+        document.querySelector(".description").textContent = data.description;
+    }
+}
+
+lecture();
